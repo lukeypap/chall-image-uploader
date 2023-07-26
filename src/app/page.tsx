3cb6b54fb78afd6011efcfdc999ba74b2923ac2b
Luke Pap <luke.pap@live.co.uk>
2023-07-26 22:39:07 +0100

    Added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import ImageUploader from "@/components/ImageUploader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-10">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-10 pb-4">
       <ImageUploader />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center text-sm opacity-50">
+      created by <b className="ml-1 mr-1">lukeypap</b> - devChallenges.io
+    </footer>
+  );
+}
